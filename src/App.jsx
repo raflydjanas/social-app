b6ea9e3c48a28b160c314cf7fc038d9b9081c315
raflydjanas <moh.raflidjans@gmail.com>
2024-02-14 20:50:08 +0800

    set up project used react and tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App;
