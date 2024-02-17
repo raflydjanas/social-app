399dd32b25f65989793235a2b47ff08235a03de2
raflydjanas <moh.raflidjans@gmail.com>
2024-02-18 00:16:41 +0800

    menjalankan fitur form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,42 @@
 import { useState } from "react";
+import axios from 'axios';
 
 
 export default function Login() {
-    const [action, setAction] = useState('Sign up')  
-    const [username, setUsername] = useState('');
-    const [userEmail, setUserEmail] = useState('');
-    const [userPassword, setUserPassword] = useState('');
-    
-    const handleSubmit = (e) => {
-        e.preventDefault(); 
+    const [action, setAction] = useState('Sign up') ; 
+    const [dataUser, setDataUser] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
 
-        if(action === 'Login') {
-            if(userEmail === '' && userPassword === '') {
-                alert('please enter your email and password');
-                return;
-            }
-        }
+    const handleChange = (e) => {
+        setDataUser({...dataUser, [e.target.name]: e.target.value });
+
+        console.log(dataUser);
     }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const baseurl = 'https://documenter.getpostman.com/view/16770115/2sA2r6ZQrt'
+
+            let url = `${baseurl}/auth/login`;
+            let paylod = {
+                username: dataUser.username,
+                email: dataUser.email,
+                password: dataUser.password
+            }
+
+            const ress = await axios.post(url, paylod);
+            console.log(ress.data);
+
+        } catch (err) {
+            console.log(err);
+        }
+
+    } 
 
     return (
         
@@ -26,25 +46,28 @@ export default function Login() {
         <div className="">
             <h2 className="text-purple text-2xl mb-6 font-semibold text-center underline">{action}</h2>
             <div className="mb-5">
-                {action === 'Login' ? <div></div>: <div>
+                {action === 'Login' ? <div></div> : <div>
                         <p className="text-purple pb-2">username</p>
                         <input type="text" placeholder="enter your username" 
                         className="py-3 px-12 bg-black text-white"
-                        value={username}
-                        onChange={(e)=>setUsername(e.target.value)}/>
+                        value={dataUser.username}
+                        onChange={handleChange}
+                        name='username'/>
                     </div>}              
             </div>
             <div className="mb-5">
                 <p className="text-purple pb-2">email</p>
                 <input type="email" placeholder="enter your email"  className="py-3 px-12 bg-black text-white"
-                 value={userEmail}
-                 onChange={(e)=>setUserEmail(e.target.value)}/>
+                 value={dataUser.email}
+                 onChange={handleChange}
+                 name='email'/>
             </div>
             <div className="mb-5">
                 <p className="text-purple pb-2">password</p>
                 <input type="password" placeholder="enter your password" className="py-3 px-12 bg-black text-white"
-                 value={userPassword}
-                 onChange={(e)=>setUserPassword(e.target.value)}/>
+                value={dataUser.password}
+                onChange={handleChange}
+                name='password'/>
             </div>   
             <div className="mt-7 flex items-center justify-around">
                 <button className={ action === 'Login' ? 'bg-gray-500 py-2 px-7 rounded-2xl' : 'bg-purple py-2 px-7 rounded-2xl' } onClick={() => {setAction('Sign up')}} > Sign UP </button>
